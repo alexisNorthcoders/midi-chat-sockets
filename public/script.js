23e1634b348dde6e9e52267ff45777f1af431bce
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-12 11:41:15 +0100

    add event listeners to keys, add function to play note using the oscilator when mouse down

--- public/script.js
+++ public/script.js
@@ -1,18 +1,19 @@
 import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
 
 const socket = io("http://192.168.4.29:3000");
-//Oscilator 
+//Oscilator
 
 const context = new AudioContext();
 const oscillators = {};
 let midi, data;
+let isMouseDown = false
 
 function frequency(note) {
-  return Math.pow(2, ((note - 69) / 12)) * 440;
+  return Math.pow(2, (note - 69) / 12) * 440;
 }
-function playNote(data){
-  console.log('play note: ', data.pitch);
-  switch(data.on) {
+function playNote(data) {
+  console.log("play note: ", data.pitch);
+  switch (data.on) {
     case 144:
       noteOn(frequency(data.pitch), data.velocity);
       break;
@@ -24,17 +25,17 @@ function playNote(data){
   function noteOn(frequency, velocity) {
     const vol = (velocity / 127).toFixed(2);
 
-    const osc = oscillators[frequency] = context.createOscillator();
-        osc.type = 'sawtooth';
-        osc.frequency.value = frequency;
-        osc.setVolume = vol;
-        osc.connect(context.destination);
-        osc.start(context.currentTime);
+    const osc = (oscillators[frequency] = context.createOscillator());
+    osc.type = "sawtooth";
+    osc.frequency.value = frequency;
+    osc.setVolume = vol;
+    osc.connect(context.destination);
+    osc.start(context.currentTime);
   }
 
   function noteOff(frequency, velocity) {
-      oscillators[frequency].stop(context.currentTime);
-      oscillators[frequency].disconnect();
+    oscillators[frequency].stop(context.currentTime);
+    oscillators[frequency].disconnect();
   }
 }
 
@@ -44,6 +45,7 @@ const messageForm = document.getElementById("form");
 const messageInput = document.getElementById("message-input");
 const joinRoomButton = document.getElementById("room-button");
 const roomInput = document.getElementById("room-input");
+const pianoKeys = document.querySelectorAll(".piano-key");
 
 // Chat sockets
 
@@ -81,8 +83,28 @@ socket.on("midiMessage", (data) => {
 
 // Audio
 
-
 // Event Listeners
+pianoKeys.forEach(key => {
+  key.addEventListener('mousedown', () => {
+    const note = key.id; 
+    const pitch = convertNoteToMIDI(note); 
+    playNote({ on: 144, pitch, velocity: 127 });
+  });
+  key.addEventListener('mouseup', () => {
+    const note = key.id; 
+    const pitch = convertNoteToMIDI(note); 
+    playNote({ on: 128, pitch, velocity: 127 });
+  });
+  key.addEventListener('mouseleave', () => {
+    const note = key.id; 
+    const pitch = convertNoteToMIDI(note); 
+    playNote({ on: 128, pitch, velocity: 127 });
+  });
+  key.addEventListener('contextmenu', e => {
+    e.preventDefault();
+  });
+
+ });
 
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -100,30 +122,19 @@ joinRoomButton.addEventListener("click", () => {
     appendMessage(message);
   });
 });
-
+// Chat functions
 function appendMessage(message) {
   const messageElement = document.createElement("div");
   messageElement.innerText = message;
   messageContainer.append(messageElement);
 }
 
-function generateKeyboard() {
-  const keyboard = document.getElementById("keyboard");
-  for (let i = 1; i <= 96; i++) {
-    const key = document.createElement("div");
-    key.classList.add("key");
-    key.dataset.note = i;
-    keyboard.appendChild(key);
-  }
-}
-
+// MIDI functions
 function onMIDISuccess(midiAccess) {
-  
   midiAccess.inputs.forEach((input) => {
     input.onmidimessage = onMIDIMessage;
   });
 }
-
 
 function onMIDIFailure(msg) {
   console.error(`Failed to get MIDI access - ${msg}`);
@@ -133,22 +144,29 @@ function onMIDIMessage(message) {
   const [command, note, velocity] = message.data;
   socket.emit("midiMessage", { command, note, velocity });
   visualizeKey(note, velocity);
-  
-const noteToPlay = {
-  on: command, 
-  pitch: note,
-  velocity: velocity
-}
-playNote(noteToPlay);
+
+  const noteToPlay = {
+    on: command,
+    pitch: note,
+    velocity: velocity,
+  };
+  playNote(noteToPlay);
 }
 
-function visualizeKey(note, velocity) {
-  const keyElement = document.querySelector(`.key[data-note='${note}']`);
-  if (velocity > 0) {
-    keyElement.classList.add("active");
-  } else {
-    keyElement.classList.remove("active");
+function convertNoteToMIDI(note) {
+  const noteMap = {
+    'C': 0, 'C#': 1, 'Db': 1, 'D': 2, 'D#': 3, 'Eb': 3,
+    'E': 4, 'F': 5, 'F#': 6, 'Gb': 6, 'G': 7, 'G#': 8, 'Ab': 8,
+    'A': 9, 'A#': 10, 'Bb': 10, 'B': 11
+  };
+  const octaveInput = 4
+  const octave = note.includes("+") ? octaveInput + 1 : octaveInput
+  
+  const noteIndex = noteMap[note.replace('+', '')]
+  if (!isNaN(octave) && noteIndex !== undefined) {
+    return octave * 12 + noteIndex + 12; 
   }
+  return NaN;
 }
 
 if (navigator.requestMIDIAccess) {
@@ -157,4 +175,3 @@ if (navigator.requestMIDIAccess) {
   console.log("Web MIDI API not supported!");
 }
 
-//generateKeyboard();
